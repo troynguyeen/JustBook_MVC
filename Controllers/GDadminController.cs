b2aaa28560e21521de1d331c387acabe81c392cf
lyquochoa <lyquochoa000@gmail.com>
2021-04-10 17:13:32 +0700

    hoa gay

---
+++ Controllers/GDadminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JustBook.Controllers
+{
+    public class GDadminController : Controller
+    {
+        // GET: GDadmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
