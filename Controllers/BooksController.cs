9e5e81aad87e7a748ea40599c1fd278527700018
lyquochoa <lyquochoa000@gmail.com>
2021-04-19 20:28:30 +0700

    hoa push code Books

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -9,11 +9,31 @@ namespace JustBook.Controllers
     public class BooksController : Controller
     {
         // GET: Books
-        public ActionResult Index()
+        public ActionResult Detail()
         {
             return View();
         }
-        public ActionResult Detail()
+        public ActionResult Books_Page1()
+        {
+            return View();
+        }
+        public ActionResult Books_Page2()
+        {
+            return View();
+        }
+        public ActionResult Books_Page3()
+        {
+            return View();
+        }
+        public ActionResult Books_Page4()
+        {
+            return View();
+        }
+        public ActionResult Books_Page5()
+        {
+            return View();
+        }
+        public ActionResult Books_Page7()
         {
             return View();
         }
