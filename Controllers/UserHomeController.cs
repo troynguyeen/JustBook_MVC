3c56334ee65f2e1f70743937a436121529220f41
Nguyen Chi Thanh <classic.nct@gmail.com>
2021-05-03 15:02:33 +0700

    chuc nang theo doi don hang cua nguoi dung

--- Controllers/UserHomeController.cs
+++ Controllers/UserHomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JustBook.Models;
+using JustBook.ViewModel;
 
 namespace JustBook.Controllers
 {
     public class UserHomeController : Controller
     {
-        // GET: UserHome
+        private DB_CT25Team23Entities db;
+        private List<OrderDetailViewModel> listOfDetail;
+        public UserHomeController()
+        {
+            db = new DB_CT25Team23Entities();
+            listOfDetail = new List<OrderDetailViewModel>();
+        }
         public ActionResult Index()
         {
             return View();
@@ -21,12 +29,72 @@ namespace JustBook.Controllers
 
         public ActionResult OrderHistory()
         {
-            return View();
+            int idKH = Int32.Parse(Session["MaKH"].ToString());
+        IEnumerable <OrderManagementModel> listOfDonHang = (from trangthai in
+               (from trangthai in db.TrangThaiDonHangs
+                orderby trangthai.MaTrangThaiDH descending
+                group trangthai by trangthai.MaDH into grp
+                select grp.OrderByDescending(x => x.MaTrangThaiDH).FirstOrDefault())
+                join dh in db.DonHangs on trangthai.MaDH equals dh.MaDH
+                where dh.MaKH == idKH
+                select new OrderManagementModel ()
+                  {
+                     
+                  MaDH = dh.MaDH,
+                  MaKH = dh.MaKH,
+                  TenNguoiNhan = dh.TenNguoiNhan,
+                  PhoneNguoiNhan = dh.PhoneNguoiNhan,
+                  DiaChiNguoiNhan = dh.DiaChiNguoiNhan,
+                  ThoiGianTao = dh.ThoiGianTao,
+                  PhuongThucThanhToan = dh.PhuongThucThanhToan,
+                  TongGiaTriDonHang = dh.TongGiaTriDonHang,
+                  TrangThaiDonHang = trangthai.TrangThai
+                  }
+           ).ToList();
+            return View(listOfDonHang);
         }
 
         public ActionResult OrderDetail()
         {
-            return View();
+            var currentId_Url = Url.RequestContext.RouteData.Values["id"];
+
+            OrderManagementModel dh_model_url = new OrderManagementModel();
+            DonHang dh = db.DonHangs.SingleOrDefault(model => model.MaDH.ToString() == currentId_Url.ToString());
+            TrangThaiDonHang trangthai = db.TrangThaiDonHangs.OrderByDescending(x => x.MaTrangThaiDH).FirstOrDefault(model => model.MaDH == dh.MaDH);
+
+            var ListOfChiTietDH = db.ChiTietDonHangs.Where(model => model.MaDonHang == dh.MaDH).ToList();
+            foreach (var chitiet in ListOfChiTietDH)
+            {
+                OrderDetailViewModel detail = new OrderDetailViewModel();
+                SanPham sanpham = db.SanPhams.FirstOrDefault(x => x.MaSP == chitiet.MaSP);
+
+                detail.MaChiTietDH = chitiet.MaChiTietDH;
+                detail.MaDonHang = dh.MaDH;
+                detail.MaSP = chitiet.MaSP;
+                detail.SoLuong = chitiet.SoLuong;
+                detail.SoLuongConLai = sanpham.SoLuong;
+                detail.DonGia = chitiet.DonGia;
+                detail.ChietKhau = chitiet.ChietKhau;
+                detail.TongTien = chitiet.TongTien;
+                detail.TenSP = sanpham.TenSP;
+                detail.LoaiSanPham = db.LoaiSanPhams.FirstOrDefault(x => x.MaLoaiSP == sanpham.MaLoaiSP).TenLoaiSP;
+                detail.ImagePath = sanpham.ImagePath;
+
+                listOfDetail.Add(detail);
+            }
+
+            dh_model_url.MaDH = dh.MaDH;
+            dh_model_url.MaKH = dh.MaKH;
+            dh_model_url.TenNguoiNhan = dh.TenNguoiNhan;
+            dh_model_url.PhoneNguoiNhan = dh.PhoneNguoiNhan;
+            dh_model_url.DiaChiNguoiNhan = dh.DiaChiNguoiNhan;
+            dh_model_url.ThoiGianTao = dh.ThoiGianTao;
+            dh_model_url.PhuongThucThanhToan = dh.PhuongThucThanhToan;
+            dh_model_url.TongGiaTriDonHang = dh.TongGiaTriDonHang;
+            dh_model_url.TrangThaiDonHang = trangthai.TrangThai;
+            dh_model_url.ChiTietDonHang = listOfDetail;
+
+            return View(dh_model_url);
         }
 
         public ActionResult TrackingState()
