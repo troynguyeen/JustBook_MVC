ea7f977df3f17f093a8f69468ab0ab5ccd2099d4
Nguyen Chi Thanh <classic.nct@gmail.com>
2021-05-07 15:07:39 +0700

    Them chuc nang chi tiet don hang user

---
+++ ViewModel/OrderStateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JustBook.ViewModel
+{
+    public class OrderStateViewModel
+    {
+        public int MaTrangThaiDH { get; set; }
+        public int MaDH { get; set; }
+        public System.DateTime ThoiGian { get; set; }
+        public string TrangThai { get; set; }
+    }
+}
